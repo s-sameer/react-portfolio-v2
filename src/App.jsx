a516f8eb630c99a4fa27deed2155321ae9e35873
s-sameer <itsnotsameer@gmail.com>
2022-10-28 05:31:58 -0230

    fixed rollup issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import About from '../components/About'
-import Skills from '../components/Skills'
-import Projects from '../components/Projects'
-import Contact from '../components/Contact'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import About from './components/About'
+import Skills from './components/Skills'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 const App = () => {
   return (
