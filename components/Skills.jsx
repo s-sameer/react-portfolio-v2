d455e458493163226902ab8396c4067d376e7d11
s-sameer <itsnotsameer@gmail.com>
2022-10-28 04:49:20 -0230

    again

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -2,10 +2,10 @@ import {motion} from "framer-motion"
 import { Swiper, SwiperSlide } from "swiper/react";
 import Skill from './Skill'
 import "swiper/css";
-import pandas from "../src/images/pandas.svg"
-import jenkins from "../src/images/jenkins.svg"
-import rust from "../src/images/rust.svg"
-import nexjs from "../src/images/nextjs-icon.svg"
+import pandas from "src/images/pandas.svg"
+import jenkins from "src/images/jenkins.svg"
+import rust from "src/images/rust.svg"
+import nexjs from "src/images/nextjs-icon.svg"
 
 function Skills() {
   return (
